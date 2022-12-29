0f21393e40f44c1b159bcc428b1d868524652da9
spinachpasta <49642352+spinachpasta@users.noreply.github.com>
2022-12-30 02:33:56 +0900

    manipulate pixels

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use opencv::{
-    highgui,
-    imgcodecs::{self, IMREAD_COLOR},
-    prelude::MatTraitConstManual,
-    Result,
-};
+use cv::core::{CV_8U, Vec3b};
+use cv::prelude::MatExprTraitConst;
+use ndarray::ArrayView3;
+use opencv as cv;
+use opencv::{highgui, imgcodecs::*, prelude::*, Result};
 
 fn main() -> Result<()> {
-    let image = imgcodecs::imread("input/rgb/front.png", IMREAD_COLOR)?;
-    let depth = imgcodecs::imread("input/depth/front.png", IMREAD_COLOR)?;
+    let image = imread("input/rgb/front.png", IMREAD_COLOR)?;
+    let depth = imread("input/depth/front.png", IMREAD_COLOR)?;
 
     let image_size = image.size()?;
     let depth_size = depth.size()?;
@@ -16,15 +15,23 @@ fn main() -> Result<()> {
         panic!("size of rgb and depth image have to be equal");
     }
 
-    // let image_pixels = image.data_bytes()?;
-    let depth_pixels = depth.data_bytes()?;
+    let mut preview =
+        (Mat::zeros(image_size.width, image_size.height, cv::core::CV_8UC3)?).to_mat()?;
 
-    let x = 10;
-    let y = 10;
-    println!("{}", depth_pixels[(x + image_size.width * y) as usize]);
+    let x = 0;
+    let y = 0;
+    // println!("{}", depth_pixels[(x + image_size.width * y) as usize]);
+    for y in 0..image_size.height - 1 {
+        for x in 0..image_size.width - 1 {
+            // let d = depth.at_2d::<u8>(y, x)?;
+            // let c = image.at_3d::<u8>(y, x, 0)?;
+            let mut p = preview.at_2d_mut::<Vec3b>(y, x)?;
+            *p = Vec3b::all(220);
+        }
+    }
 
     highgui::named_window("hello opencv!", 0)?;
-    highgui::imshow("hello opencv!", &depth)?;
+    highgui::imshow("hello opencv!", &preview)?;
     highgui::wait_key(10000)?;
     Ok(())
 }
