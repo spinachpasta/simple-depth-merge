1d64297e76cfecda9acbae53dfda95317cd09028
spinachpasta <49642352+spinachpasta@users.noreply.github.com>
2022-12-30 16:09:16 +0900

    check feature points

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,21 @@ use cv::core::{Vec3b, CV_8U};
 use cv::prelude::MatExprTraitConst;
 use ndarray::ArrayView3;
 use opencv as cv;
-use opencv::{highgui, imgcodecs::*, imgproc::*, prelude::*, Result};
+use opencv::{core::*, highgui, imgcodecs::*, imgproc::*, imgproc::*, prelude::*, Result};
 
 fn main() -> Result<()> {
+    let feature_front: [Point_<i32>; 4] = [
+        Point_::<i32>::new(231, 290),
+        Point_::<i32>::new(315, 210),
+        Point_::<i32>::new(251, 363),
+        Point_::<i32>::new(252, 130),
+    ];
+    let feature_side: [Point_<i32>; 3] = [
+        Point_::<i32>::new(1, 1),
+        Point_::<i32>::new(1, 1),
+        Point_::<i32>::new(1, 1),
+    ];
+
     let image = imread("input/rgb/front.png", IMREAD_COLOR)?;
     let depth_rgb = imread("input/depth/front.png", IMREAD_COLOR)?;
     let mut depth = Mat::default();
@@ -40,8 +52,26 @@ fn main() -> Result<()> {
         }
     }
 
-    highgui::named_window("hello opencv!", 0)?;
-    highgui::imshow("hello opencv!", &preview)?;
+    let mut preview_front = Mat::default();
+    image.copy_to(&mut preview_front)?;
+    for fp in feature_front {
+        circle(
+            &mut preview_front,
+            fp,
+            5,
+            Scalar_::new(255.0, 0.0, 0.0, 1.0),
+            -1,
+            1,
+            0,
+        )?;
+    }
+
+    highgui::named_window("pseudo right", 0)?;
+    highgui::imshow("pseudo right", &preview)?;
+
+    highgui::named_window("front", 0)?;
+    highgui::imshow("front", &preview_front)?;
+
     highgui::wait_key(10000)?;
     Ok(())
 }
