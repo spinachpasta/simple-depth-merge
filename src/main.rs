8eac35d78ccb3aaaefac92fe8ae6e35b6fff5840
spinachpasta <49642352+spinachpasta@users.noreply.github.com>
2022-12-31 01:01:36 +0900

    fix clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use cv::core::{Vec3b, CV_8U};
+#![warn(clippy::pedantic)]
+#![allow(clippy::wildcard_imports)]
+use cv::core::Vec3b;
 use cv::prelude::MatExprTraitConst;
-use ndarray::ArrayView3;
 use opencv as cv;
-use opencv::{core::*, highgui, imgcodecs::*, imgproc::*, imgproc::*, prelude::*, Result};
+use opencv::{core::*, highgui, imgcodecs::*, imgproc::*, Result};
 use std::collections::HashMap;
 
 struct DepthView {
@@ -15,28 +16,29 @@ struct DepthView {
 
 impl DepthView {
     pub fn from_filename(
-        rgb_path: String,
-        depth_path: String,
+        rgb_path: &str,
+        depth_path: &str,
         features: HashMap<String, Point_<i32>>,
     ) -> Result<DepthView> {
-        let image = imread(&rgb_path, IMREAD_COLOR)?;
-        let depth_rgb = imread(&depth_path, IMREAD_COLOR)?;
+        let image = imread(rgb_path, IMREAD_COLOR)?;
+        let depth_rgb = imread(depth_path, IMREAD_COLOR)?;
         let mut depth = Mat::default();
         cvt_color(&depth_rgb, &mut depth, COLOR_BGR2GRAY, 0)?;
-        Ok(DepthView::new(image, depth, features)?)
+        DepthView::new(image, depth, features)
     }
     pub fn new(rgb: Mat, depth: Mat, features: HashMap<String, Point_<i32>>) -> Result<DepthView> {
         let image_size = rgb.size()?;
         let depth_size = depth.size()?;
-        if image_size.width != depth_size.width && image_size.height != depth_size.height {
-            panic!("size of rgb and depth image have to be equal");
-        }
+        assert!(
+            !(image_size.width != depth_size.width && image_size.height != depth_size.height),
+            "size of rgb and depth image have to be equal"
+        );
         let ret = DepthView {
-            rgb: rgb,
-            depth: depth,
+            rgb,
+            depth,
             width: image_size.width,
             height: image_size.height,
-            features: features,
+            features,
         };
         Ok(ret)
     }
@@ -75,8 +77,8 @@ impl DepthView {
             for x in 0..self.width - 1 {
                 let d = self.depth.at_2d::<u8>(y, x)?;
                 let c = self.rgb.at_2d::<Vec3b>(y, x)?;
-                let z: i32 = *d as i32;
-                let mut p = preview.at_2d_mut::<Vec3b>(y, z)?;
+                let z: i32 = i32::from(*d);
+                let p = preview.at_2d_mut::<Vec3b>(y, z)?;
                 *p = *c;
             }
         }
@@ -84,11 +86,11 @@ impl DepthView {
             let y = fp.y;
             let x = fp.x;
             let d = self.depth.at_2d::<u8>(y, x)?;
-            let z: i32 = *d as i32;
+            let z: i32 = i32::from(*d);
 
             circle(
                 &mut preview,
-                Point_ { x: z, y: y },
+                Point_ { x: z, y },
                 5,
                 Scalar_::new(255.0, 0.0, 0.0, 1.0),
                 -1,
@@ -98,7 +100,7 @@ impl DepthView {
             put_text(
                 &mut preview,
                 fpname,
-                Point_ { x: z, y: y },
+                Point_ { x: z, y },
                 FONT_HERSHEY_DUPLEX,
                 1.0,
                 Scalar_::new(255.0, 128.0, 128.0, 1.0),
@@ -112,18 +114,6 @@ impl DepthView {
 }
 
 fn main() -> Result<()> {
-    let feature_front: [Point_<i32>; 4] = [
-        Point_::<i32>::new(231, 290),
-        Point_::<i32>::new(315, 210),
-        Point_::<i32>::new(251, 363),
-        Point_::<i32>::new(252, 130),
-    ];
-    let feature_side: [Point_<i32>; 3] = [
-        Point_::<i32>::new(1, 1),
-        Point_::<i32>::new(1, 1),
-        Point_::<i32>::new(1, 1),
-    ];
-
     let mut feature_front = HashMap::new();
     feature_front.insert("nose".to_string(), Point_::<i32>::new(231, 290));
     feature_front.insert("eye".to_string(), Point_::<i32>::new(315, 210));
@@ -131,8 +121,8 @@ fn main() -> Result<()> {
     feature_front.insert("hair".to_string(), Point_::<i32>::new(252, 130));
 
     let front = DepthView::from_filename(
-        "input/rgb/front.png".to_string(),
-        "input/depth/front.png".to_string(),
+        "input/rgb/front.png",
+        "input/depth/front.png",
         feature_front,
     )?;
 
