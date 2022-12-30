77e821d75b0e272d3991fb27199c5e2ef785fba4
spinachpasta <49642352+spinachpasta@users.noreply.github.com>
2022-12-30 20:49:46 +0900

    struct implementation

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,17 @@ struct DepthView {
 }
 
 impl DepthView {
+    pub fn from_filename(
+        rgb_path: String,
+        depth_path: String,
+        features: HashMap<String, Point_<i32>>,
+    ) -> Result<DepthView> {
+        let image = imread(&rgb_path, IMREAD_COLOR)?;
+        let depth_rgb = imread(&depth_path, IMREAD_COLOR)?;
+        let mut depth = Mat::default();
+        cvt_color(&depth_rgb, &mut depth, COLOR_BGR2GRAY, 0)?;
+        Ok(DepthView::new(image, depth, features)?)
+    }
     pub fn new(rgb: Mat, depth: Mat, features: HashMap<String, Point_<i32>>) -> Result<DepthView> {
         let image_size = rgb.size()?;
         let depth_size = depth.size()?;
@@ -31,7 +42,8 @@ impl DepthView {
     }
 
     pub fn debug_features(&self) -> Result<Mat> {
-        let mut preview = Mat::default();
+        let mut preview = self.rgb.try_clone()?;
+        
         for (fpname, fp) in &self.features {
             circle(
                 &mut preview,
@@ -57,13 +69,11 @@ impl DepthView {
         Ok(preview)
     }
     pub fn debug_side(&self) -> Result<Mat> {
-        let image_size = self.rgb.size()?;
-        let depth_size = self.depth.size()?;
         let mut preview =
-            (Mat::zeros(image_size.width, image_size.height, cv::core::CV_8UC3)?).to_mat()?;
+            (Mat::zeros(self.width, self.height, cv::core::CV_8UC3)?).to_mat()?;
 
-        for y in 0..image_size.height - 1 {
-            for x in 0..image_size.width - 1 {
+        for y in 0..self.height - 1 {
+            for x in 0..self.width - 1 {
                 let d = self.depth.at_2d::<u8>(y, x)?;
                 let c = self.rgb.at_2d::<Vec3b>(y, x)?;
                 let z: i32 = *d as i32;
@@ -88,60 +98,23 @@ fn main() -> Result<()> {
         Point_::<i32>::new(1, 1),
     ];
 
-    let image = imread("input/rgb/front.png", IMREAD_COLOR)?;
-    let depth_rgb = imread("input/depth/front.png", IMREAD_COLOR)?;
-    let mut depth = Mat::default();
-    cvt_color(&depth_rgb, &mut depth, COLOR_BGR2GRAY, 0)?;
-
-    let image_size = image.size()?;
-    let depth_size = depth.size()?;
-
-    if image_size.width != depth_size.width && image_size.height != depth_size.height {
-        panic!("size of rgb and depth image have to be equal");
-    }
-
-    let mut preview =
-        (Mat::zeros(image_size.width, image_size.height, cv::core::CV_8UC3)?).to_mat()?;
-
-    let x = 0;
-    let y = 0;
-    for y in 0..image_size.height - 1 {
-        for x in 0..image_size.width - 1 {
-            let d = depth.at_2d::<u8>(y, x)?;
-            let c = image.at_2d::<Vec3b>(y, x)?;
-
-            let mut z: i32 = -(*d as i32) + 256;
-            if z < 0 {
-                z = 0;
-            }
-            if z >= image_size.width {
-                z = image_size.width - 1;
-                continue;
-            }
-            let mut p = preview.at_2d_mut::<Vec3b>(y, z)?;
-            *p = *c;
-        }
-    }
-
-    let mut preview_front = Mat::default();
-    image.copy_to(&mut preview_front)?;
-    for fp in feature_front {
-        circle(
-            &mut preview_front,
-            fp,
-            5,
-            Scalar_::new(255.0, 0.0, 0.0, 1.0),
-            -1,
-            1,
-            0,
-        )?;
-    }
+    let mut feature_front = HashMap::new();
+    feature_front.insert("nose".to_string(), Point_::<i32>::new(231, 290));
+    feature_front.insert("eye".to_string(), Point_::<i32>::new(315, 210));
+    feature_front.insert("chin".to_string(), Point_::<i32>::new(251, 363));
+    feature_front.insert("hair".to_string(), Point_::<i32>::new(252, 130));
+    
+    let front = DepthView::from_filename(
+        "input/rgb/front.png".to_string(),
+        "input/depth/front.png".to_string(),
+        feature_front
+    )?;
 
     highgui::named_window("pseudo right", 0)?;
-    highgui::imshow("pseudo right", &preview)?;
+    highgui::imshow("pseudo right", &front.debug_side()?)?;
 
     highgui::named_window("front", 0)?;
-    highgui::imshow("front", &preview_front)?;
+    highgui::imshow("front", &front.debug_features()?)?;
 
     highgui::wait_key(10000)?;
     Ok(())
