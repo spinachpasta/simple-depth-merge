ed6cdcde214b4a52d2fed64bfd26d4d1389cc943
spinachpasta <49642352+spinachpasta@users.noreply.github.com>
2023-01-03 00:39:22 +0900

    linear regression

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 #![allow(clippy::wildcard_imports)]
 use cv::core::Vec3b;
 use cv::prelude::MatExprTraitConst;
-use lstsq;
+
+use na::Matrix1x4;
 use nalgebra as na;
 use opencv as cv;
 use opencv::{core::*, highgui, imgcodecs::*, imgproc::*, Result};
@@ -99,10 +100,8 @@ impl DepthView {
 
         for y in 0..self.height - 1 {
             for x in 0..self.width - 1 {
-                // let d = depth.at_2d::<u8>(y, x)?;
-                let d = self.get_depth_round(&x, &y);
                 let c = self.rgb.at_2d::<Vec3b>(y, x)?;
-                let z: i32 = self.get_depth_round(&x, &y);
+                let z = depth[(y as usize, x as usize)].round() as i32;
                 if z < 0 || self.width <= z {
                     continue;
                 }
@@ -113,7 +112,7 @@ impl DepthView {
         for (fpname, fp) in &self.features {
             let y = fp.y;
             let x = fp.x;
-            let z = self.get_depth_round(&x, &y);
+            let z = depth[(y as usize, x as usize)].round() as i32;
 
             circle(
                 &mut preview,
@@ -144,32 +143,50 @@ impl DepthView {
         // m.set_row(0, na::RowVector4())
         let fpnames = self.match_features(other);
 
-        let mut x = na::OMatrix::<f64, na::Dynamic, na::U4>::zeros(fpnames.len());
+        let mut x = na::OMatrix::<f64, na::Dynamic, na::U3>::zeros(fpnames.len());
         let mut y = na::OVector::<f64, na::Dynamic>::zeros(fpnames.len());
         let mut index: usize = 0;
+
         for (index, fpname) in fpnames.into_iter().enumerate() {
-            let origin = other.features.get(fpname).unwrap();
+            let origin = self.features.get(fpname).unwrap();
             let target = other.features.get(fpname).unwrap();
-            x.set_column(
-                index,
-                &na::Matrix4x1::<f64>::new(
-                    f64::from(origin.x),
-                    f64::from(origin.y),
-                    self.get_depth(&origin.x, &origin.y),
-                    1.0,
-                ),
-            );
-            y.set_column(index, &na::Vector1::<f64>::new(f64::from(target.x)));
+            x[(index, 0)] = f64::from(origin.x);
+            x[(index, 1)] = f64::from(origin.y);
+            x[(index, 2)] = self.get_depth(&origin.x, &origin.y);
+            y[(index, 0)] = f64::from(target.x);
         }
-        let m = lstsq::lstsq(&x, &y, 1e-14).unwrap().solution;
-        Ok(m)
+        let xmean = x.row_mean();
+        let ymean = y.row_mean();
+        //subtract x
+        let mut xx = x.clone();
+        for (mut column, coeff) in xx.column_iter_mut().zip(xmean.iter()) {
+            column.add_scalar_mut(*coeff)
+        }
+        //subtract y
+        let mut yy = y.clone();
+        for (mut column, coeff) in yy.column_iter_mut().zip(ymean.iter()) {
+            column.add_scalar_mut(*coeff)
+        }
+
+        println!("f{xx} {yy}");
+        let m = lstsq::lstsq(&xx, &yy, 1e-14).unwrap().solution;
+        let w = ymean - xmean * m;
+        let m1 = na::Matrix4x1::<f64>::new(m.x, m.y, m.z, w.x);
+        Ok(m1)
     }
 
     pub fn calibrate_z_linear(&self, other: &DepthView) -> Result<na::DMatrix<f64>> {
-        let mut calibrated = na::DMatrix::<f64>::zeros(1, 1);
+        let mut calibrated = na::DMatrix::<f64>::zeros(self.height as usize, self.width as usize);
 
+        let m = self.get_zmatrix(other)?;
+        println!("!{m}");
         for y in 0..self.height - 1 {
-            for x in 0..self.width - 1 {}
+            for x in 0..self.width - 1 {
+                // println!("{x} {y}");
+                let d =
+                    (Matrix1x4::<f64>::new(0.0, y as f64, self.get_depth(&x, &y), 1.0) * m);
+                calibrated[(y as usize, x as usize)] = d[(0, 0)];
+            }
         }
         Ok(calibrated)
     }
@@ -193,13 +210,13 @@ fn main() -> Result<()> {
     feature_front.insert("nose".to_string(), Point_::<i32>::new(231, 290));
     feature_front.insert("eye".to_string(), Point_::<i32>::new(315, 210));
     feature_front.insert("chin".to_string(), Point_::<i32>::new(251, 363));
-    feature_front.insert("hair".to_string(), Point_::<i32>::new(252, 130));
+    // feature_front.insert("hair".to_string(), Point_::<i32>::new(252, 130));
 
     let mut feature_side = HashMap::new();
     feature_side.insert("nose".to_string(), Point_::<i32>::new(178, 277));
     feature_side.insert("eye".to_string(), Point_::<i32>::new(280, 222));
     feature_side.insert("chin".to_string(), Point_::<i32>::new(219, 357));
-    feature_side.insert("hair".to_string(), Point_::<i32>::new(217, 134));
+    // feature_side.insert("hair".to_string(), Point_::<i32>::new(217, 134));
 
     let front = DepthView::from_filename(
         "input/rgb/front.png",
@@ -213,17 +230,17 @@ fn main() -> Result<()> {
     highgui::named_window("pseudo right", 0)?;
     highgui::imshow("pseudo right", &front.debug_side()?)?;
 
-    highgui::named_window("front", 0)?;
-    highgui::imshow("front", &front.debug_features()?)?;
+    // highgui::named_window("front", 0)?;
+    // highgui::imshow("front", &front.debug_features()?)?;
 
     highgui::named_window("side", 0)?;
     highgui::imshow("side", &side.debug_features()?)?;
 
-    // highgui::named_window("corrected", 0)?;
-    // highgui::imshow(
-    //     "corrected",
-    //     &front.debug_depth(&front.get_calibrated(&side)?)?,
-    // )?;
+    highgui::named_window("corrected", 0)?;
+    highgui::imshow(
+        "corrected",
+        &front.debug_depth(&front.calibrate_z_linear(&side)?)?,
+    )?;
 
     highgui::wait_key(10000)?;
     Ok(())
